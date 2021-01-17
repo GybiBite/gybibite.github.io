e4ad1f98e042ba6741e5f090217a99d50b26f467
nooby117 <44080726+nooby117@users.noreply.github.com>
2021-01-16 22:23:21 -0800

    Create Upload script.js

---
+++ Upload script.js
@@ -0,0 +1,18 @@
+/* file upload system: must be implemented into spread sheet, create a name, url, like value, dislike value, view value, and a uploader value.
+
+step one, check if user is signed in, if no, upload as anon
+
+if (userName) {
+	
+}
+
+step two have them select a file, must implement a sorter to determine file type and proceed from there with corresponding board
+
+step three have them input a name, must have a value, this will have a new spot on the spread sheet
+
+step four, create a unique url, series of random numbers and letters or just increment by 1 as a form of serial number
+
+step five, copy page from a template and fill in all data with user added data
+
+step six, have them implement description data and tags
+
